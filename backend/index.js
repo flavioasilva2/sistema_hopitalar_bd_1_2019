3ec5691bcafa47a92bc44d181029344184a2fed0
Flavio Silva <flavio.asilva@gmail.com>
2019-05-22 14:10:40 -0300

    Adding expressjs package to backend.

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
